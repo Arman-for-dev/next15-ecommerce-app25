39ca071455da8591cbc855189d330f0f9738d5d9
Arman-for-dev <armanfordev59@gmail.com>
2025-07-30 10:40:47 +0600

    password & comparepassword

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,2 +1,14 @@
+import bcrypt from "bcryptjs"
 import NextAuth from "next-auth"
 export const { handlers, auth, signIn, signOut } = NextAuth({ providers: [  ] })
+
+export async function hashedPassword(password: string){
+    const saltround = await bcrypt.genSalt(10);
+    return await bcrypt.hashSync(password, saltround);
+}
+
+export async function comparedPassword(password: string, hashedPassword: string){
+    return await bcrypt.compareSync(password, hashedPassword);
+} {
+    
+}
